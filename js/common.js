8bb384c6e94126d0162b162a6e49f7b939f854df
lukawitch <lukawitch@naver.com>
2018-07-18 17:07:15 +0900

    mobiletest

--- js/common.js
+++ js/common.js
@@ -2,6 +2,7 @@ if (window.outerWidth>window.outerHeight) {
 
   $(function(){
     $('#back').css('margin','auto');
+	 $('#bg1').css('margin','auto');
   });
 }/*가로 길이가 길 때 back기본속성유지하고 margin:auto값만 줘서
 가운데정렬만합니다*/
@@ -9,6 +10,8 @@ else{
  $(function(){
    $('#back').css('width',window.outerWidth);
    $('#back').css('height',window.outerHeight);
+    $('#bg1').css('width',window.outerWidth);
+   $('#bg1').css('height',window.outerHeight);
  });
 }
 /*그게아니라면  device의 가로세로길이를 가져와서 가로세로를 변경합니다*/
